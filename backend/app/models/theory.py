e70c40045f5d17177195ff40225c9e54ecfc2c73
dlaurianti <dlaurianti@uiowa.edu>
2023-09-26 11:38:17 -0500

    feat: intervals up to 2 octaves

--- backend/app/models/theory.py
+++ backend/app/models/theory.py
@@ -15,11 +15,11 @@ class note_names(Enum):
     B = 11
 
 class note_accidentals(Enum):
-    bb = -2
-    b = -1
-    n = 0
-    s = 1
-    ss = 2
+    d = -2 # double flat
+    b = -1 # flat
+    n = 0 # natural
+    s = 1 # sharp
+    x = 2 # double sharp
 
 class intervals(Enum):
     p1 = 0
@@ -53,3 +53,33 @@ class intervals(Enum):
     a7 = 12
 
     P8 = 12
+
+    d9 = 12
+    m9 = 13
+    M9 = 14
+    a9 = 15
+
+    d10 = 14
+    m10 = 15
+    M10 = 16
+    a10 = 17
+
+    d11 = 16
+    P11 = 17
+    a11 = 18
+    
+    d12 = 18
+    P12 = 19
+    a12 = 20
+
+    d13 = 19
+    m13 = 20
+    M13 = 21
+    a13 = 22
+
+    d14 = 21
+    m14 = 22
+    M14 = 23
+    a14 = 24
+
+    P15 = 24
